63e790b49bb9fd182733917322f0edf111c52c9d
Ron Newcomb <pscion@yahoo.com>
2023-11-22 09:51:18 -0800

    ideas

--- src/engine2.ts
+++ src/engine2.ts
@@ -8,64 +8,6 @@ export const database: Database = [];
 console.info("enter listing() to show the database");
 (window as any).listing = () => database.forEach(rule => console.log(JSON.stringify(rule)));
 
-// class FunctorIterator {
-//   current: TupleItem | typeof NOMORE = NOMORE;
-//   i = 0;
-//   isDone = false;
-//   subIter: FunctorIterator | undefined;
-
-//   constructor(private areTheseTrue: Tuple[], private scope: Environment, private db: Database) {
-//     this.first();
-//   }
-
-//   first(): FunctorIterator {
-//     this.i = 0;
-//     this.isDone = false;
-//     this.next();
-//     return this;
-//   }
-
-//   next(): FunctorIterator {
-//     if (this.areTheseTrue.length == 0 || this.isDone) {
-//       this.isDone = true;
-//       this.current = NOMORE;
-//       return this;
-//     }
-
-//     const first = this.areTheseTrue[0];
-//     const rest = this.areTheseTrue.slice(1);
-
-//     for (; this.db.length > this.i; this.i++) {
-//       const rule = this.db[this.i];
-
-//       const nextEnvironment = this.scope.unify(first, rule.head); // try to unify the first of goals[] with this rule's head
-//       if (nextEnvironment == null) continue; // no unify? try next rule in the db
-
-//       const nextGoals = rule.body ? rule.body.concat(rest) : rest;
-
-//       if (!this.subIter) this.subIter = new FunctorIterator(nextGoals, nextEnvironment, this.db);
-//       if (this.subIter.isDone) {
-//         this.subIter = undefined;
-//         continue;
-//       }
-//       return this.subIter;
-//     }
-
-//     this.isDone = true;
-//     this.current = NOMORE;
-//     return this;
-//   }
-
-//   rest(): TupleItem[] {
-//     const vals = [];
-//     while (!this.isDone) {
-//       const val = this.next();
-//       if (val.current != NOMORE) vals.push(val.current);
-//     }
-//     return vals;
-//   }
-// }
-
 interface GraphNode {
   //parent?: GraphNode;
   queryToProve: Tuple;
@@ -90,13 +32,22 @@ const enum Direction {
   Succeeded,
 }
 
-export function goer(current: GraphNode): Direction {
-  //if (current.dbIndex == undefined) current.dbIndex = -1;
-  //let state: Direction = Direction.Succeeded;
+function succeeds(database: Database, queryToProve?: Tuple): boolean {
+  const node: GraphNode = queryToProve ? { queryToProve, database, dbIndex: -1 } : previousRun;
+  const result = goer(node);
+  previousRun = node;
+  return result === Direction.Succeeded;
+}
 
+function goer(current: GraphNode): Direction {
+  // on a backtrack, start over
   on_backtracking: do {
     // find a rule in database that unifies with current .queryToProve (unification sets .vars)
     if (!current.vars) {
+      // clean this
+      current.querysToProve = [];
+
+      // find a rule in the db that unifies with our current.queryToProve
       let nextrule: Rule | undefined;
       while (!current.vars) {
         // get next rule to try
@@ -107,76 +58,33 @@ export function goer(current: GraphNode): Direction {
         if (!nextrule) return Direction.Failing;
 
         // does it unify?
-        current.vars = unify(current.vars, { tupleitem: current.queryToProve }, { tupleitem: nextrule.rule.head! });
+        current.vars = unify(current.vars, current.queryToProve, nextrule.head!);
       }
 
-      // found a rule that unified. If it had conditions, we will try those conditions
-      if (nextrule?.rule.body)
-        current.querysToProve = nextrule.rule.body.map<GraphNode>(t => ({
-          queryToProve: t,
-          database: current.database,
-          dbIndex: -1,
-        }));
+      // we found a rule that unified. If it had conditions, we will try those conditions
+      if (nextrule!.query)
+        for (const query of nextrule!.query)
+          current.querysToProve.push({
+            queryToProve: query,
+            database: current.database,
+            dbIndex: -1,
+          });
     }
 
     // check children recursively, regardless whether this is a replay or they're fresh
-    if (current.querysToProve)
-      for (let query of current.querysToProve) {
-        // child returns success or failure.
-        const state = goer(query);
-        if (state == Direction.Succeeded) continue;
+    for (const query of current.querysToProve!) {
+      // child returns success or failure.
+      const state = goer(query);
+      if (state == Direction.Succeeded) continue;
 
-        // if child didn't succeed, then nextrule doesn't succeed. Reset and restart with a new db rule
-        current.vars = undefined;
-        current.querysToProve = [];
-        continue on_backtracking;
-      }
+      // if child didn't succeed, then nextrule doesn't succeed. Reset and restart with a new db rule
+      current.vars = undefined;
+      continue on_backtracking;
+    }
   } while (false);
 
   return Direction.Succeeded; // all children succeeded, so, I do too.
 }
-
-// export function go(root: GraphNode): GraphNode {
-//   let state: "go" | "backtracking" = "go";
-//   const location: number[] = []; // from the end of this array, from root, choose the nth child
-//   const current = location.reduceRight((sum, each) => sum.querysToProve[each], root);
-
-//   while (true) {
-//     if (current.dbIndex == undefined) current.dbIndex = -1;
-
-//     // ?
-//     if (state == "backtracking") current.vars = null;
-
-//     if (!current.vars) {
-//       let nextrule: Rule | undefined = undefined;
-//       while (!current.vars) {
-//         current.dbIndex++;
-//         nextrule = current.database[current.dbIndex];
-//         if (!nextrule) return (state = "backtracking");
-//         current.vars = unify(current.vars, { tupleitem: current.queryToProve }, { tupleitem: nextrule.rule.head! });
-//       }
-
-//       const query = nextrule?.rule.body || [];
-//       if (query.length) {
-//         //current.queryIndex = 0;
-//         current.querysToProve = query.map(
-//           bodytuple =>
-//             ({
-//               parent: current,
-//               queryToProve: bodytuple,
-//               database: current.database,
-//               dbIndex: 0,
-//             } as GraphNode)
-//         );
-//         location.push(0);
-//       } else {
-//         location[0]++;
-//         if (location[0] >= (current.parent?.querysToProve.length ?? 0)) location.shift();
-//       }
-//     }
-//   }
-//   return root;
-// }
 
 /*
 to get the Value of a tupleitem,
@@ -209,57 +117,44 @@ interface Literal {
     num?: number;
     boo?: boolean;
   };
-  variable?: undefined; // for typechecking
-  tuple?: undefined; // for typechecking
+  variable?: never; // for typechecking
+  tuple?: never; // for typechecking
 }
 interface Variable {
   variable: {
     bareword: string;
   };
-  literal?: undefined; // for typechecking
-  tuple?: undefined; // for typechecking
+  literal?: never; // for typechecking
+  tuple?: never; // for typechecking
 }
-interface TupleItem {
-  tupleitem: Literal | Variable | Tuple;
-}
+type TupleItem = Literal | Variable | Tuple;
 interface Tuple {
   tuple: TupleItem[];
-  variable?: undefined; // for typechecking
-  literal?: undefined; // for typechecking
+  variable?: never; // for typechecking
+  literal?: never; // for typechecking
 }
 interface Rule {
-  rule: {
-    head: Tuple;
-    body: Tuple[];
-  };
+  head: Tuple;
+  query: Tuple[];
 }
-
 interface InputFile {
   lines: Rule[];
 }
 
-const sample: InputFile = {
+const sample2: InputFile = {
   lines: [
     {
-      rule: {
-        head: {
-          tuple: [
-            { tupleitem: { literal: { bareword: "drive" } } },
-            { tupleitem: { literal: { str: "bob" } } },
-            {
-              tupleitem: {
-                tuple: [
-                  {
-                    tupleitem: { literal: { bareword: "downtown" } },
-                  },
-                ],
-              },
-            },
-            { tupleitem: { variable: { bareword: "car" } } },
-          ],
-        },
-        body: [],
+      head: {
+        tuple: [
+          { literal: { bareword: "drive" } },
+          { literal: { str: "bob" } },
+          {
+            tuple: [{ literal: { bareword: "downtown" } }],
+          },
+          { variable: { bareword: "car" } },
+        ],
       },
+      query: [],
     },
   ],
 };
@@ -276,43 +171,38 @@ function newTuple(items: TupleItem[]): Tuple {
   return { tuple: items };
 }
 
-function valueOf(tupleitem: TupleItem, scope: Scope): TupleItem {
-  const item = tupleitem.tupleitem;
-  if (item.literal) return tupleitem;
-  if (item.tuple)
-    return {
-      tupleitem: newTuple(item.tuple.map(it => valueOf(it, scope))),
-    };
-  // else item.variable  // else item.bound
+function valueOf(item: TupleItem, scope: Scope): TupleItem {
+  if (item.literal) return item;
+  if (item.tuple) return newTuple(item.tuple.map(it => valueOf(it, scope)));
   const isBoundVar = scope[item.variable.bareword];
-  return isBoundVar ? isBoundVar[0] : tupleitem;
+  return isBoundVar ? isBoundVar[0] : item;
 }
 
 function unify(scope: Scope | undefined, a: TupleItem, b: TupleItem): Scope | undefined {
   if (scope == null) return undefined;
-  const x = valueOf(a, scope).tupleitem;
-  const y = valueOf(b, scope).tupleitem;
-  if (x.variable) return newScope(scope, x.variable.bareword, { tupleitem: y });
-  if (y.variable) return newScope(scope, y.variable.bareword, { tupleitem: x });
+  const x = valueOf(a, scope);
+  const y = valueOf(b, scope);
+  if (x.variable) return newScope(scope, x.variable.bareword, y);
+  if (y.variable) return newScope(scope, y.variable.bareword, x);
   if (x.literal || y.literal) return x.literal == y.literal ? scope : undefined;
   if (x.tuple.length != y.tuple.length) return undefined;
   for (let i = 0; i < x.tuple.length; i++) scope = unify(scope, x.tuple[i], y.tuple[i]);
   return scope;
 }
 
-let previousRun = new FunctorIterator([], {}, database);
+let previousRun: GraphNode | undefined;
 
 export function processLine(rule: Rule): void {
-  if (rule.rule.head.tuple[0].tupleitem.literal?.bareword != "more") {
+  let result = false;
+  if (rule.head.tuple[0].literal?.bareword != "more") {
     if (rule == null) return;
     printEcholine(JSON.stringify(rule));
-    if (!rule.rule.body || rule.rule.body.length == 0) {
+    if (rule.head) {
       database.push(rule);
       printAnswerline("Memorized.\n");
       return;
     }
-    previousRun = new FunctorIterator(rule.rule.body || [], {}, database);
-  }
-  const result = previousRun.next();
-  printAnswerline(result.current == NOMORE ? "No." : result.current.print());
+    result = succeeds(database, rule.query!);
+  } else result = succeeds(database);
+  printAnswerline(!result ? "No." : previousRun?.vars.print());
 }
--- src/nearley/projama.ne
+++ src/nearley/projama.ne
@@ -7,11 +7,11 @@
 
 main      -> line:+                                             {% d => ({ lines: d.flat(4)}) %}
 line      -> rule | query                                      #{% d => ({ lines: d[0] }) %}
-rule      -> _ tuple (__ "if" __ tuplelist):? "." _             {% d => ({ rule: { head: d[1], body: d.slice(2).flat(2).filter(x => !!x && typeof x !== 'string')[0]?.tuplelist ?? [] } }) %}
+rule      -> _ tuple (__ "if" __ tuplelist):? "." _             {% d => ({ head: d[1], query: d.slice(2).flat(2).filter(x => !!x && typeof x !== 'string')[0]?.tuplelist ?? [] }) %}
 query     -> _ tuplelist "?" _                                  {% d => ({ query: d[1] }) %}
 tuplelist -> tuple (_ "," _ tuple):*                            {% d => ({ tuplelist: d.flat(5).filter(x => !!x && typeof x !== 'string') }) %}
 tuple     -> "[" _  tupleitem ( _ "," _ tupleitem ):* _ "]"     {% d => ({ tuple:     d.flat(5).filter(x => !!x && typeof x !== 'string') }) %}
-tupleitem -> ( variable | literal | tuple )                     {% d => ({ tupleitem: d[0][0] }) %}
+tupleitem -> ( variable | literal | tuple )                    #{% d => ({ tupleitem: d[0][0] }) %}
 literal   -> ( string | number | boolean | bareword )           {% d => ({ literal: d[0][0] }) %}
 string    -> ["] [^"]:* ["]                                     {% d => ({ str: d[1].join('') }) %}
 number    -> [0-9]:+ ("." [0-9]:+):?                            {% d => ({ num: d.flat(3).join('') }) %}
