ad1e2faf46f2667308bb069982d3d4490ae453a3
Ron Newcomb <pscion@yahoo.com>
2023-11-23 22:32:27 -0800

    rename folders

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,8 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from "@rollup/plugin-commonjs"
 
 export default {
-    // input: "tmp/main.js",
-    input: "tmp/test2/initial.test.js",
+    input: "tmp/test/initial.test.js",
     output: {
         file: "./tmp/prolog.js",
         format: "iife",
